387b7796a76b22f466997b088e21f4354ea926df
Kamie Sullivan <kamiesull10@gmail.com>
2024-10-17 11:30:03 -0700

    Added attributes to ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -15,11 +15,15 @@ namespace ContosoCrafts.WebSite.Models
     public class ProductModel
     {
         public string Id { get; set; }
-        
+
         [JsonPropertyName("img")]
         public string Image { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public Season BestSeason { get; set; }
+        public string Currency { get; set; }
+        public string TimeZone { get; set; }
+        public string[] Attractions { get; set; }
         public int[] Ratings { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
