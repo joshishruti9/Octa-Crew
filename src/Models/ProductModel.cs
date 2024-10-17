293bc31235254fae6aa2bba7816313834cf705f8
Kamie Sullivan <kamiesull10@gmail.com>
2024-10-17 11:18:44 -0700

    Removed Maker and Url from ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -6,11 +6,9 @@ namespace ContosoCrafts.WebSite.Models
     public class ProductModel
     {
         public string Id { get; set; }
-        public string Maker { get; set; }
         
         [JsonPropertyName("img")]
         public string Image { get; set; }
-        public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
