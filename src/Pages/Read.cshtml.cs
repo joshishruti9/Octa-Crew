622d297aef7c7f23501e92199abcc08820bd22ae
Nathan Swetzof <nswetzof@gmail.com>
2024-10-18 11:32:02 -0700

    Add constructor to ReadModel in Read.cshtml.cs

--- src/Pages/Read.cshtml.cs
+++ src/Pages/Read.cshtml.cs
@@ -1,3 +1,4 @@
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,17 @@ namespace ContosoCrafts.WebSite.Pages
 {
     public class ReadModel : PageModel
     {
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
+        public ReadModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
         public void OnGet()
         {
         }
