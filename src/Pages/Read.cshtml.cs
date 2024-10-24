38c31dd1171e70e1750da230ca755a7246832d45
kamiesull <kamiesull10@gmail.com>
2024-10-24 10:01:37 -0700

    Added comments to Read.cshtml.cs

--- src/Pages/Read.cshtml.cs
+++ src/Pages/Read.cshtml.cs
@@ -7,14 +7,18 @@ using System.Linq;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Displays city details based on city's ID
+    /// </summary>
     public class ReadModel : PageModel
     {
+        // Accesses city data from database
         public JsonFileProductService ProductService { get; }
 
         /// <summary>
         /// Default Constructor
         /// </summary>
-        /// <param name="productService"></param>
+        /// <param name="productService">Set to ProductService</param>
         public ReadModel(JsonFileProductService productService)
         {
             ProductService = productService;
@@ -26,7 +30,7 @@ namespace ContosoCrafts.WebSite.Pages
         /// <summary>
         /// REST get request
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">ID of the city to be displayed</param>
         public void OnGet(string id)
         {
             Product = ReadData(id);
@@ -36,7 +40,7 @@ namespace ContosoCrafts.WebSite.Pages
         /// Return the city matching a given ID
         /// </summary>
         /// <param name="id">The city ID</param>
-        /// <returns></returns>
+        /// <returns>ProductModel object for the given city, or null if not found</returns>
         private ProductModel ReadData(string id)
         {
             return ProductService.GetProducts().FirstOrDefault(x => x.Id.Equals(id));
