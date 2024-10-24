e4fff9a2c8be46b3bf92e9e423434483d2399635
kamiesull <kamiesull10@gmail.com>
2024-10-24 09:16:19 -0700

    Added comments to ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -3,7 +3,9 @@ using System.Text.Json.Serialization;
 
 namespace ContosoCrafts.WebSite.Models
 {
-    // enum to restrict values in BestSeason
+    /// <summary>
+    /// enum to restrict values in BestSeason
+    /// </summary>
     public enum Season
     {
         Spring = 0,
@@ -12,20 +14,43 @@ namespace ContosoCrafts.WebSite.Models
         Winter = 3
     }
 
+    /// <summary>
+    /// Model representing a city
+    /// </summary>
     public class ProductModel
     {
+        // ID for the city
         public string Id { get; set; }
 
+        // Array of image URLs for the city
         [JsonPropertyName("imgs")]
         public string[] Images { get; set; }
+        
+        // Name of the city
         public string Title { get; set; }
+        
+        // Description about the city
         public string Description { get; set; }
+        
+        // Best season to visit city (spring, summer, fall, or winter)
         public Season? BestSeason { get; set; }
+        
+        // Which currency is used in the city
         public string Currency { get; set; }
+        
+        // Which time zone the city is in (format is GMT+/-X)
         public string TimeZone { get; set; }
+        
+        // List of the top attractions in the city
         public string[] Attractions { get; set; }
+        
+        // Ratings added by users for how nice the city is to visit
         public int[] Ratings { get; set; }
 
+        /// <summary>
+        /// Represents the ProductModel as a json string
+        /// </summary>
+        /// <returns>A JSON string representation of ProductModel</returns>
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
  
