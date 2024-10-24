35f90e731cc22a5dd7bd1c68506603897e6e0bae
kamiesull <kamiesull10@gmail.com>
2024-10-24 09:26:50 -0700

    Added comments to Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -6,20 +6,35 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Model for handling the error page and displaying error information
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
+        // ID for HTTP request
         public string RequestId { get; set; }
 
+        /// <summary>
+        /// Only shows the request ID if it exists (is not null or empty)
+        /// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        // Records logs
         private readonly ILogger<ErrorModel> _logger;
 
+        /// <summary>
+        /// Initializer constructor
+        /// </summary>
+        /// <param name="logger">Logs error information</param>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// When the error page is requested, RequestId is set
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
