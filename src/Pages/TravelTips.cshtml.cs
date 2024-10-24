ab2818870f5fd1238acdc38700ebd4b9bbc52bfd
kamiesull <kamiesull10@gmail.com>
2024-10-24 10:03:15 -0700

    Added comments to TravelTips.cshtml.cs

--- src/Pages/TravelTips.cshtml.cs
+++ src/Pages/TravelTips.cshtml.cs
@@ -3,8 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Model for Travel Tips page
+    /// </summary>
     public class TravelTipsModel : PageModel
     {
+        /// <summary>
+        /// Called when page is accessed
+        /// </summary>
         public void OnGet()
         {
         }
