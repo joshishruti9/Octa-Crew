a3fdedf7ec9ddcba24bcf413683c701c6c8f9d87
kamiesull <kamiesull10@gmail.com>
2024-10-24 09:55:50 -0700

    Added comments to Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,15 +3,26 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Model for Privacy page
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        // Logs information, errors, etc. for Privacy page
         private readonly ILogger<PrivacyModel> _logger;
 
+        /// <summary>
+        /// Constructor initializes _logger
+        /// </summary>
+        /// <param name="logger">Set to _logger</param>
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Called when the page is accessed
+        /// </summary>
         public void OnGet()
         {
         }
