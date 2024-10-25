6c8c8c80c92ae18dd0db9029d44d530e92cd2373
Shruti Joshi <joshi2312shruti@gmail.com>
2024-10-26 02:05:26 +0530

    Added changes in IndexPageTests.cs to add test initialize setup

--- UnitTests/Pages/IndexPageTests.cs
+++ UnitTests/Pages/IndexPageTests.cs
@@ -9,5 +9,21 @@ namespace UnitTests.Pages
 
     public class IndexPageTests
     {
+        // Database MiddleTier
+        #region TestSetup
+        public static IndexPageModel pageModel;
+        /// <summary>
+        /// Initialize of Test
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<IndexPageModel>>();
+            pageModel = new IndexPageModel(MockLoggerDirect, TestHelper.ProductService)
+            {
+            };
+        }
+
+        #endregion TestSetup
     }
 }
