e3a3b9c3562a31871ab5be14d1239cf5be5f715d
Shruti Joshi <joshi2312shruti@gmail.com>
2024-10-25 10:26:40 +0530

    Added file IndexPageTests.cs for index test cases

---
+++ UnitTests/Pages/IndexPageTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using ContosoCrafts.WebSite.Pages;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace UnitTests.Pages
+{
+
+    public class IndexPageTests
+    {
+    }
+}
