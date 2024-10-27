9ddc17eb1825910bcdc6ffe67946bec171073d71
Shruti Joshi <joshi2312shruti@gmail.com>
2024-10-28 03:09:17 +0530

    Added default constructor in Update.cshtml.cs

--- src/Pages/Update.cshtml.cs
+++ src/Pages/Update.cshtml.cs
@@ -1,12 +1,24 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     public class UpdateModel : PageModel
     {
-        public void OnGet()
+        // Accesses city data from database
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService">Set to ProductService</param>
+        public UpdateModel(JsonFileProductService productService)
         {
+            ProductService = productService;
         }
+
     }
 }
