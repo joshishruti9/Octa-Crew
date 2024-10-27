76524b69d8044a7723d6388c6612fe6e62a2483e
Nathan Swetzof <nswetzof@gmail.com>
2024-10-27 10:57:57 -0700

    Removed unused using statements from JsonfileProductServiceTests.cs

--- UnitTests/Services/JsonFileProductServiceTests.cs
+++ UnitTests/Services/JsonFileProductServiceTests.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
-using ContosoCrafts.WebSite.Models;
-using ContosoCrafts.WebSite.Pages;
 using NUnit.Framework;
 
 namespace UnitTests.Services
