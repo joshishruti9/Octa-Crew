bdb5f74ec48770e05417c742b90be865b7cf4de8
Shruti Joshi <joshi2312shruti@gmail.com>
2024-10-29 03:17:15 +0530

    Added test setup for Error.cshtml.cs in Error.cshtml.Tests.cs

---
+++ UnitTests/Pages/Error.cshtml.Tests.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+using Microsoft.Extensions.Logging;
+
+using NUnit.Framework;
+
+using Moq;
+
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages.Error
+{
+    public class ErrorTests
+    {
+        #region TestSetup
+        public static ErrorModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            var MockLoggerDirect = Mock.Of<ILogger<ErrorModel>>();
+
+            pageModel = new ErrorModel(MockLoggerDirect)
+            {
+                PageContext = TestHelper.PageContext,
+                TempData = TestHelper.TempData,
+            };
+        }
+
+        #endregion TestSetup
+    }
+}
