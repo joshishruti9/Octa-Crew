c48ca7ae396499115dc494af721bbcfecca24145
Nathan Swetzof <nswetzof@gmail.com>
2024-10-29 18:34:33 -0700

    Added unit test for OnGet in Index.cshtml.Tests.cs

--- UnitTests/Pages/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Index.cshtml.Tests.cs
@@ -4,9 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Linq;
+
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages;
+
 namespace UnitTests.Pages
 {
-    internal class Index
+    /// <summary>
+    /// Unit testing class for Index page model
+    /// </summary>
+    public class IndexTests
     {
+        #region TestSetup
+
+        // page model for the Index (cities) page
+        public static IndexModel pageModel;
+
+        /// <summary>
+        /// Called before each test is called.
+        /// Sets up necessary test context or variables
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            pageModel = new IndexModel(TestHelper.ProductService);
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        [Test]
+        public void OnGet_Valid_Default_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+            var result = TestHelper.ProductService.GetProducts();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, pageModel.Products.ToList().Any());
+        }
+
+        #endregion OnGet
     }
 }
