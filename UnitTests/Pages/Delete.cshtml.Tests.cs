8a4468ddc91ff56327588065e2f6ec88c741220f
kamiesull <kamiesull10@gmail.com>
2024-10-31 11:33:31 -0700

    Set up tests for delete page in Delete.cshtml.Tests.cs

--- UnitTests/Pages/Delete.cshtml.Tests.cs
+++ UnitTests/Pages/Delete.cshtml.Tests.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using ContosoCrafts.WebSite.Pages;
 
 namespace UnitTests.Pages
 {
-	internal class Delete
+	/// <summary>
+	/// Unit testing class for Delete page model
+	/// </summary>
+	public class DeleteTests
 	{
+		#region TestSetup
+
+		public static DeleteModel pageModel;
+		public JsonFileProductService ProductService;
+
+		/// <summary>
+		/// Called before each test is called.
+		/// Sets up necessary test context or variables
+		/// </summary>
+		[SetUp]
+		public void Setup()
+		{
+			// Use a mock or test helper for ProductService
+			ProductService = TestHelper.ProductService;
+			pageModel = new DeleteModel(ProductService);
+		}
+
+		#endregion TestSetup
 	}
 }
