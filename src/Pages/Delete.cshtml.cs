71b982c18cde25a62ac07885d5f5a4a93f147fcc
kamiesull <kamiesull10@gmail.com>
2024-10-31 11:04:32 -0700

    Added comments to Delete.cshtml.cs

--- src/Pages/Delete.cshtml.cs
+++ src/Pages/Delete.cshtml.cs
@@ -3,8 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Model for Delete CRUDi page
+    /// </summary>
     public class DeleteModel : PageModel
     {
+        /// <summary>
+        /// Called when the page is accessed
+        /// </summary>
         public void OnGet()
         {
         }
