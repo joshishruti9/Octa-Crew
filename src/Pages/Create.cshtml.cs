914ee149f4e56fa3f9117154f7d4c6e0e2c59231
Nathan Swetzof <nswetzof@gmail.com>
2024-11-01 09:29:06 -0700

    Added CreateModel methods to add a city to the database in Create.cshtml.cs

--- src/Pages/Create.cshtml.cs
+++ src/Pages/Create.cshtml.cs
@@ -1,12 +1,49 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
 
 namespace ContosoCrafts.WebSite.Pages
 {
     public class CreateModel : PageModel
     {
+        // Accesses city data from database
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="productService"></param>
+        public CreateModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public ProductModel Product { get; set; }
+
+        /// <summary>
+        /// REST Get request initializes the data
+        /// </summary>
         public void OnGet()
         {
+            Product = ProductService.CreateData();
+        }
+
+        /// <summary>
+        /// REST Post request saves the filled in data to the database
+        /// </summary>
+        /// <returns>The result of the Post request</returns>
+        public IActionResult OnPost()
+        {
+            var dataSet = ProductService.GetAllData();
+            dataSet = dataSet.Append(Product);
+
+            ProductService.SaveData(dataSet);
+
+            return RedirectToPage("./Index");
         }
     }
 }
