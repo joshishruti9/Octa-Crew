66ce197eeab2c81e7f8e4edb87eae19571bd4ee1
kamiesull <kamiesull10@gmail.com>
2024-11-01 18:23:03 -0700

    Set up tests for TravelTips model in TravelTips.cshtml.Tests.cs

---
+++ UnitTests/Pages/TravelTips.cshtml.Tests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages
+{
+	/// <summary>
+	/// Unit testing class for TravelTips page model
+	/// </summary>
+	public class TravelTipsTests
+	{
+		#region TestSetup
+
+		public static TravelTipsModel pageModel;
+
+		/// <summary>
+		/// Called before each test is called.
+		/// Sets up necessary test context or variables
+		/// </summary>
+		[SetUp]
+		public void Setup()
+		{
+			pageModel = new TravelTipsModel();
+		}
+
+		#endregion TestSetup
+
+		#region OnGet
+		[Test]
+		public void OnGet_Valid_Activity_Set_Should_Have_Valid_State()
+		{
+			// Arrange
+
+			// Act
+			pageModel.OnGet();
+
+			// Reset
+
+			// Assert
+			Assert.That(pageModel.ModelState.IsValid, Is.EqualTo(true));
+		}
+
+		#endregion OnGet
+	}
+}
