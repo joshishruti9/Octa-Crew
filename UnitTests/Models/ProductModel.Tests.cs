b7fa7bb85a0906ae7ce1d47e7d83b719ed9a49b1
kamiesull <kamiesull10@gmail.com>
2024-11-04 11:06:17 -0800

    Added GetCityRating_Invalid_Test_Ratings_Null_Should_Return_0_True test method to ProductModel.Tests.cs

--- UnitTests/Models/ProductModel.Tests.cs
+++ UnitTests/Models/ProductModel.Tests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ContosoCrafts.WebSite.Models;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +15,23 @@ namespace UnitTests.Models
     {
 		#region GetCityRating
 
-		
+		/// <summary>
+		/// Test that a call of GetCityRating() on a city with no ratings should return 0
+		/// </summary>
+		[Test]
+		public void GetCityRating_Invalid_Test_Ratings_Null_Should_Return_0_True()
+		{
+			// Arrange
+			var data = new ProductModel();
+
+			// Act
+			var result = data.GetCityRating();
+
+			// Reset
+
+			// Assert
+			Assert.AreEqual(result, 0);
+		}
 
 		#endregion
 	}
