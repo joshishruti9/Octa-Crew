1a79aa370ec5a2cea64dd9b9f10b5403d45b6a6d
kamiesull <kamiesull10@gmail.com>
2024-11-05 11:23:34 -0800

    Created file JsonFileTravelTipService.cs

---
+++ src/Services/JsonFileTravelTipService.cs
@@ -0,0 +1,9 @@
+﻿namespace ContosoCrafts.WebSite.Services
+{
+	/// <summary>
+	/// Service class with methods for managing Travel Tips data
+	/// </summary>
+	public class JsonFileTravelTipService
+	{
+	}
+}
