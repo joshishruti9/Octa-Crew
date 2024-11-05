541c0e357cc5aaaacef0af29f0fe1b298c67bdee
kamiesull <kamiesull10@gmail.com>
2024-11-05 11:26:00 -0800

    Added WebHostEnvironment to JsonFileTravelTipService.cs

--- src/Services/JsonFileTravelTipService.cs
+++ src/Services/JsonFileTravelTipService.cs
@@ -1,9 +1,13 @@
-﻿namespace ContosoCrafts.WebSite.Services
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace ContosoCrafts.WebSite.Services
 {
 	/// <summary>
 	/// Service class with methods for managing Travel Tips data
 	/// </summary>
 	public class JsonFileTravelTipService
 	{
+		// Web hosting environment used
+		public IWebHostEnvironment WebHostEnvironment { get; }
 	}
 }
