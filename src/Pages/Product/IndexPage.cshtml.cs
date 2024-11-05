7873c3d3da454b4d7881be5709988a7451f37972
Shruti Joshi <joshi2312shruti@gmail.com>
2024-11-06 05:24:15 +0530

    Move IndexPage.cshtml.cs in product folder

---
+++ src/Pages/Product/IndexPage.cshtml.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    /// <summary>
+    /// Model for cities page
+    /// </summary>
+    public class IndexPageModel : PageModel
+    {
+        // Keeps logs for IndexModel
+        private readonly ILogger<IndexPageModel> _logger;
+
+        /// <summary>
+        /// Initializer constructor
+        /// </summary>
+        /// <param name="logger">Set to _logger</param>
+        /// <param name="productService">Set to ProductService</param>
+        public IndexPageModel(ILogger<IndexPageModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        // Service for getting data from database
+        public JsonFileProductService ProductService { get; }
+
+        // List of cities and their data
+        public IEnumerable<ProductModel> Products { get; set; }
+
+        /// <summary>
+        /// Fills Products with data when page is accessed
+        /// </summary>
+        public void OnGet()
+        {
+            Products = ProductService.GetAllData();
+        }
+    }
+}
