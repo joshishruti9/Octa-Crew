e28b767a0106c0111d03a391b5f299be1787c99b
kamiesull <kamiesull10@gmail.com>
2024-11-05 12:06:32 -0800

    Added test for CreateData in JsonFileTravelTipServiceTests.cs

--- UnitTests/Services/JsonFileTravelTipServiceTests.cs
+++ UnitTests/Services/JsonFileTravelTipServiceTests.cs
@@ -1,4 +1,7 @@
 ﻿
+using ContosoCrafts.WebSite.Models;
+using NUnit.Framework;
+
 namespace UnitTests.Services
 {
 	/// <summary>
@@ -6,5 +9,30 @@ namespace UnitTests.Services
 	/// </summary>
 	public class JsonFileTravelTipServiceTests
 	{
+		#region CreateData
+
+		/// <summary>
+		/// Test that CreateData returns a TravelTipsModel object with the expected default values
+		/// </summary>
+		[Test]
+		public void CreateData_Valid_Default_Should_Return_Default_Fields()
+		{
+			// Arrange
+			var data = new TravelTipsModel
+			{
+				Id = System.Guid.NewGuid().ToString(),
+				Title = "Enter Tip Name",
+				Description = "Enter Tip Description",
+			};
+
+			// Act
+			var result = TestHelper.TravelTipService.CreateData();
+
+			// Assert
+			Assert.AreEqual(data.Title, result.Title);
+			Assert.AreEqual(data.Description, result.Description);
+		}
+
+		#endregion CreateData
 	}
 }
