c5a916146fb20ce99031d63c3ec1388777a90630
Shruti Joshi <joshi2312shruti@gmail.com>
2024-11-05 06:57:44 +0530

    Adding ConfirmationPageModel in ConfirmationPage.cshtml.cs

---
+++ src/Pages/ConfirmationPage.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// Confirmation Model for Delete Confirmation page
+    /// </summary>
+    public class ConfirmationPageModel : PageModel
+    {
+    }
+}
