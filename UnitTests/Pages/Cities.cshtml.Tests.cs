d90d9b4617ebf2dbdd056e78e5bedd35026352ec
Priyanshu Desai <pdesai1@seattleu.edu>
2024-11-05 20:17:44 -0800

    Configured the code in Unit Test accordingly in Cities.cshtml.Tests.cs

---
+++ UnitTests/Pages/Cities.cshtml.Tests.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages;
+
+namespace UnitTests.Pages
+{
+    /// <summary>
+    /// Unit testing class for Index page model
+    /// </summary>
+    public class CitiesTests
+    {
+        #region TestSetup
+
+        // page model for the Index (cities) page
+        public  CitiesModel pageModel;
+
+        /// <summary>
+        /// Called before each test is called.
+        /// Sets up necessary test context or variables
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            pageModel = new CitiesModel(TestHelper.ProductService);
+        }
+
+        #endregion TestSetup
+
+        #region OnGet
+
+        [Test]
+        public void OnGet_Valid_Default_Should_Return_Products()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(true, pageModel.Products.ToList().Any());
+        }
+
+        #endregion OnGet
+    }
+}
