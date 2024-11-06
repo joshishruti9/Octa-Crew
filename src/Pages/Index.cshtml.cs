c1b98a7f683e923102dcf7bcedf7a6a807d520fd
Priyanshu Desai <pdesai1@seattleu.edu>
2024-11-05 20:22:21 -0800

    Changed the code accordingly in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -1,38 +1,12 @@
-﻿using System.Collections.Generic;
-
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
-using ContosoCrafts.WebSite.Models;
-using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
-    /// <summary>
-    /// Model for cities page
-    /// </summary>
     public class IndexModel : PageModel
     {
-        /// <summary>
-        /// Initializer constructor
-        /// </summary>
-        /// <param name="productService">Set to ProductService</param>
-        public IndexModel(JsonFileProductService productService)
-        {
-            ProductService = productService;
-        }
-
-        // Service for getting data from database
-        public JsonFileProductService ProductService { get; }
-        
-        // List of cities and their data
-        public IEnumerable<ProductModel> Products { get; private set; }
-
-        /// <summary>
-        /// Fills Products with data when page is accessed
-        /// </summary>
         public void OnGet()
         {
-            Products = ProductService.GetAllData();
         }
     }
 }
