f7eca2e3c79f03277f70addb56c8c1dd9cb0f144
Shruti Joshi <joshi2312shruti@gmail.com>
2024-11-06 06:52:55 +0530

    Added ProductsController.Tests.cs to add ProductsController Tests

--- UnitTests/Controllers/ProductsController.Tests.cs
+++ UnitTests/Controllers/ProductsController.Tests.cs
@@ -16,64 +16,6 @@ namespace UnitTests.Controllers
     /// </summary>
     public class ProductsControllerTests
     {
-        #region TestSetup
-        
-        public JsonFileProductService ProductService;
-        public ProductsController ProductsController;
-
-        /// <summary>
-        /// Called before each test is called.
-        /// Sets up necessary test context or variables
-        /// </summary>
-        [SetUp]
-        public void Setup()
-        {
-            // Use a mock or test helper for ProductService
-            ProductService = TestHelper.ProductService;
-            ProductsController = new ProductsController(ProductService);
-        }
-
-        #endregion TestSetup
-
-        #region Get
-
-        /// <summary>
-        /// Test that Get correctly gets all Product from database
-        /// </summary>
-        [Test]
-        public void Get_Valid_Request_Should_Return_AllProducts()
-        {
-            //Arrange
-            var actualProducts = ProductService.GetAllData();
-            // Act
-            var result = ProductsController.Get();
-
-            // Assert  
-            Assert.AreEqual(result.ToList().Count,actualProducts.ToList().Count);
-        }
-
-        #endregion Get
-
-        #region Patch
-
-        /// <summary>
-        /// Test that Patch send ok response after adding rating to product
-        /// </summary>
-        [Test]
-        public void Patch_Valid_Request_Should_Return_HTTP_Status_Ok()
-        {
-            //Arrange 
-            RatingRequest ratingRequest = new RatingRequest();
-            ratingRequest.ProductId = "Paris";
-            ratingRequest.Rating = 5;
-
-            // Act
-            var result = ProductsController.Patch(ratingRequest);
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(typeof(OkResult), result.GetType());
-        }
-        #endregion Patch
     }
+
 }
