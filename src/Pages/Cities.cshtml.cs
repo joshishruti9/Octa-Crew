815adecd1b1dd48b109368ec99c5ea38febf6d66
Priyanshu Desai <pdesai1@seattleu.edu>
2024-11-05 20:15:43 -0800

    Configured the code for CitiesModel in Cities.cshtml.cs

---
+++ src/Pages/Cities.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+    /// <summary>
+    /// Model for cities page
+    /// </summary>
+    public class CitiesModel : PageModel
+    {
+        /// <summary>
+        /// Initializer constructor
+        /// </summary>
+        /// <param name="productService">Set to ProductService</param>
+        public CitiesModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // Service for getting data from database
+        public JsonFileProductService ProductService { get; }
+        
+        // List of cities and their data
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
+        /// Fills Products with data when page is accessed
+        /// </summary>
+        public void OnGet()
+        {
+            Products = ProductService.GetAllData();
+        }
+    }
+}
