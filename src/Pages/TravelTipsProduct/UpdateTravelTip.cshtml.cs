41f02529e8c5511d0e9805a658faaedb8e911afd
kamiesull <kamiesull10@gmail.com>
2024-11-07 13:09:14 -0800

    Created UpdateTravelTip.cshtml and UpdateTravelTip.cshtml.cs

---
+++ src/Pages/TravelTipsProduct/UpdateTravelTip.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.TravelTipsProduct
+{
+    public class UpdateTravelTipModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
