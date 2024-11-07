0b4e1bad7878337fb3030c497acc13dcb932e70e
kamiesull <kamiesull10@gmail.com>
2024-11-07 13:11:02 -0800

    Added comments to UpdateTravelTip.cshtml.cs

--- src/Pages/TravelTipsProduct/UpdateTravelTip.cshtml.cs
+++ src/Pages/TravelTipsProduct/UpdateTravelTip.cshtml.cs
@@ -3,8 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages.TravelTipsProduct
 {
+    /// <summary>
+    /// Model for the Update Travel Tip page allows user to update values for a travel tip in the travel tips database
+    /// </summary>
     public class UpdateTravelTipModel : PageModel
     {
+        /// <summary>
+        /// Called when the page is accessed
+        /// </summary>
         public void OnGet()
         {
         }
