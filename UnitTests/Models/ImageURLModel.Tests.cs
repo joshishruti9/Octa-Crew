7573604d43b45228800fcbfc67d9ca1e69b7c54e
kamiesull <kamiesull10@gmail.com>
2024-11-08 10:37:24 -0800

    Added Set_URL_Invalid_Null_Should_Return_Error test to ImageURLModel.Tests.cs

--- UnitTests/Models/ImageURLModel.Tests.cs
+++ UnitTests/Models/ImageURLModel.Tests.cs
@@ -1,4 +1,9 @@
 ﻿
+using ContosoCrafts.WebSite.Models;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace UnitTests.Models
 {
 	/// <summary>
@@ -6,5 +11,35 @@ namespace UnitTests.Models
 	/// </summary>
 	public class ImageURLModelTests
 	{
+		#region URL
+
+		/// <summary>
+		/// Checks that setting the URL attribute to null causes a validation error
+		/// </summary>
+		[Test]
+		public void Set_URL_Invalid_Null_Should_Return_Error()
+		{
+			// Arrange
+			var data = new ImageURLModel
+			{
+				URL = null
+			};
+			var validationResults = new List<ValidationResult>();
+
+			// Act
+			bool result = Validator.TryValidateObject(
+				data, new ValidationContext(data), validationResults, true
+			);
+
+			// Reset
+
+			// Assert
+			Assert.AreEqual(false, result);
+			Assert.AreEqual(true, validationResults.Exists(
+				vr => vr.ErrorMessage == "URL is required"
+			));
+		}
+
+		#endregion URL
 	}
 }
