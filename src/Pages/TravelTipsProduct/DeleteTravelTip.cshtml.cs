9c1bf007381e87abdc851e070e1ac68fdb0a434f
Nathan Swetzof <nswetzof@gmail.com>
2024-11-08 09:25:59 -0800

    Renamed model class in DeleteTravelTip.cshtml.cs

--- src/Pages/TravelTipsProduct/DeleteTravelTip.cshtml.cs
+++ src/Pages/TravelTipsProduct/DeleteTravelTip.cshtml.cs
@@ -1,9 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+
 
 namespace ContosoCrafts.WebSite.Pages.TravelTipsProduct
 {
-    public class IndexModel : PageModel
+    public class DeleteTravelTipModel : PageModel
     {
         public void OnGet()
         {
