4ae005a640b2b11f7904787c3f8794212ad6b989
Nathan Swetzof <nswetzof@gmail.com>
2024-11-08 10:14:49 -0800

    Added setup code to DeleteTravelTip.cshtml.Tests.cs

--- UnitTests/Pages/TravelTipsProduct/DeleteTravelTip.cshtml.Tests.cs
+++ UnitTests/Pages/TravelTipsProduct/DeleteTravelTip.cshtml.Tests.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using NUnit.Framework;
+using System.Linq;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Pages.TravelTipsProduct;
+
 namespace UnitTests.Pages.TravelTipsProduct
 {
+    /// <summary>
+    /// Unit testing class for the delete page model for travel tips
+    /// </summary>
     class DeleteTravelTip
     {
-    }
+		#region TestSetup
+
+		// page model for the CRUDi travel tips delete page
+		public static DeleteTravelTipModel pageModel;
+
+		/// <summary>
+		/// Called before each test is called.
+		/// Sets up necessary test context or variables
+		/// </summary>
+		[SetUp]
+        public void Setup()
+        {
+            pageModel = new DeleteTravelTipModel(TestHelper.TravelTipService);
+        }
+
+		#endregion TestSetup
+	}
 }
