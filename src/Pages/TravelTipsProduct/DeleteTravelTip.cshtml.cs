26a42ad2c4e5e8734b7f5d6a0af7fcbdd7b5ac74
Nathan Swetzof <nswetzof@gmail.com>
2024-11-08 09:56:18 -0800

    Added OnGet method to DeleteTravelTip.cshtml.cs

--- src/Pages/TravelTipsProduct/DeleteTravelTip.cshtml.cs
+++ src/Pages/TravelTipsProduct/DeleteTravelTip.cshtml.cs
@@ -8,6 +8,7 @@ namespace ContosoCrafts.WebSite.Pages.TravelTipsProduct
 {
     public class DeleteTravelTipModel : PageModel
     {
+        // accesses travel tips data from the database
         public JsonFileTravelTipService TravelTipService { get; set; }
 
         /// <summary>
@@ -19,5 +20,26 @@ namespace ContosoCrafts.WebSite.Pages.TravelTipsProduct
             TravelTipService = travelTipService; 
         }
 
+        // the data to show
+        public TravelTipsModel TravelTips { get; set; }
+
+        /// <summary>
+        /// REST GET request
+        /// </summary>
+        /// <param name="id">The id of the travel tip to delete</param>
+        public void OnGet(string id)
+        {
+            TravelTips = ReadData(id);
+        }
+
+        /// <summary>
+        /// Return the travel tip matching a given ID
+        /// </summary>
+        /// <param name="id">The travel tip ID</param>
+        /// <returns>TravelTipsModel object for the given city, or null if not found</returns>
+        public TravelTipsModel ReadData(string id)
+        {
+            return TravelTipService.GetAllData().FirstOrDefault(x => x.Id.Equals(id));
+        }
     }
 }
