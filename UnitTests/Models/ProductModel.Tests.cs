7064b46a8542a3eab4f212a2bb99350019caa7fd
kamiesull <kamiesull10@gmail.com>
2024-11-08 13:59:22 -0800

    Fixed ProductModel creation in Set_Images_Valid_Should_Be_Validated in ProductModel.Tests.cs

--- UnitTests/Models/ProductModel.Tests.cs
+++ UnitTests/Models/ProductModel.Tests.cs
@@ -191,12 +191,27 @@ namespace UnitTests.Models
 			// Arrange
 			var data = new ProductModel()
 			{
+				Id = System.Guid.NewGuid().ToString(),
 				Images = new string[]
 				{
 					"https://images.pexels.com/photos/1308940/pexels-photo-1308940.jpeg",
 					"https://images.pexels.com/photos/2363/france-landmark-lights-night.jpg",
-					"https://images.pexels.com/photos/161901/paris-sunset-france-monument-161901.jpeg"
-				}
+					"https://images.pexels.com/photos/161901/paris-sunset-france-monument-161901.jpeg",
+				},
+				Title = "Enter City Name",
+				Description = "Enter City Description",
+				BestSeason = null,
+				Currency = "Enter Currency",
+				TimeZone = "Enter Time Zone",
+				Attractions = new string[3]
+				{
+					"Enter an Attraction",
+					"Enter an Attraction",
+					"Enter an Attraction"
+				},
+				Cost = 0,
+				TravelTime = 0.0,
+				Ratings = null
 			};
 			var validationResults = new List<ValidationResult>();
 
