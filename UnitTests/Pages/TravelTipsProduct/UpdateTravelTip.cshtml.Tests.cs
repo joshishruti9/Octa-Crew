75c0161da55c3a850b93a76076e77428595508dc
kamiesull <kamiesull10@gmail.com>
2024-11-07 21:09:23 -0800

    Added TestSetup to UpdateTravelTip.cshtml.Tests.cs

--- UnitTests/Pages/TravelTipsProduct/UpdateTravelTip.cshtml.Tests.cs
+++ UnitTests/Pages/TravelTipsProduct/UpdateTravelTip.cshtml.Tests.cs
@@ -1,4 +1,9 @@
 ﻿
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Pages.TravelTipsProduct;
+using ContosoCrafts.WebSite.Services;
+using NUnit.Framework;
+
 namespace UnitTests.Pages.TravelTipsProduct
 {
 	
@@ -7,5 +12,29 @@ namespace UnitTests.Pages.TravelTipsProduct
 	/// </summary>
 	public class UpdateTravelTipTests
 	{
+		#region TestSetup
+
+		// Model for Update page for TravelTip
+		public static UpdateTravelTipModel pageModel;
+
+		// Service for accessing and modifying data in database
+		public JsonFileTravelTipService TravelTipService;
+
+		/// <summary>
+		/// Called before each test is called.
+		/// Sets up necessary test context or variables
+		/// </summary>
+		[SetUp]
+		public void Setup()
+		{
+			// Use a mock or test helper for TravelTipService
+			TravelTipService = TestHelper.TravelTipService;
+			pageModel = new UpdateTravelTipModel(TravelTipService)
+			{
+				TravelTip = new TravelTipsModel()
+			};
+		}
+
+		#endregion TestSetup
 	}
 }
