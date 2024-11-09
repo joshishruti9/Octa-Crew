61b9ef27150fea710a39832efd2f5fbd6c8f2262
Nathan Swetzof <nswetzof@gmail.com>
2024-11-08 16:05:51 -0800

    Added validation for the Description property in TravelTipsModel.cs

--- src/Models/TravelTipsModel.cs
+++ src/Models/TravelTipsModel.cs
@@ -11,12 +11,15 @@ namespace ContosoCrafts.WebSite.Models
 		public string Id { get; set; }
 
 		// Title of tip
-		[Required(ErrorMessage = "Title must not be empty")]
+		[Required(ErrorMessage = "Title should not be empty")]
 		[StringLength (maximumLength: 100, 
 			ErrorMessage = "The Title should have a length less than {1} characters")]
 		public string Title { get; set; }
 
 		// Description of tip
+		[Required(ErrorMessage = "Description should not be empty")]
+		[StringLength(maximumLength: 5000, MinimumLength = 1,
+			ErrorMessage = "The Description should have a length less than {1} characters")]
 		public string Description { get; set; }
 	}
 }
