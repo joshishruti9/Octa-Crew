4a3c06b603d6e42a8194cfa828dd78c7747a3c41
kamiesull <kamiesull10@gmail.com>
2024-11-09 11:01:14 -0800

    Added Set_Cost_Invalid_Less_Than_0_Should_Cause_Validation_Error to ProductModel.Tests.cs

--- UnitTests/Models/ProductModel.Tests.cs
+++ UnitTests/Models/ProductModel.Tests.cs
@@ -1206,6 +1206,55 @@ namespace UnitTests.Models
 
         #endregion Ratings
 
+        #region Cost
+
+        /// <summary>
+        /// Setting Cost to a negative number should cause a validation error
+        /// </summary>
+        [Test]
+        public void Set_Cost_Invalid_Less_Than_0_Should_Cause_Validation_Error()
+        {
+            // Arrange
+            var data = new ProductModel()
+            {
+                Id = System.Guid.NewGuid().ToString(),
+                Images = new string[]
+                {
+                    "https://images.pexels.com/photos/1308940/pexels-photo-1308940.jpeg",
+                    "https://images.pexels.com/photos/2363/france-landmark-lights-night.jpg",
+                    "https://images.pexels.com/photos/161901/paris-sunset-france-monument-161901.jpeg",
+                },
+                Title = "Enter City Name",
+                Description = "Enter City Description",
+                BestSeason = null,
+                Currency = "CUR",
+                TimeZone = "GMT-00",
+                Attractions = new string[3]
+                {
+                    "Enter an Attraction",
+                    "Enter an Attraction",
+                    "Enter an Attraction"
+                },
+                Cost = -1,
+                TravelTime = 0.0,
+                Ratings = null
+            };
+            var validationResults = new List<ValidationResult>();
+
+            // Act
+            bool result = Validator.TryValidateObject(
+                data, new ValidationContext(data), validationResults, true
+            );
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(false, result);
+            Assert.AreEqual("Cost should be between 0 and 9999", validationResults[0].ErrorMessage);
+        }
+
+        #endregion Cost
+
         #region GetCityRating
 
         /// <summary>
