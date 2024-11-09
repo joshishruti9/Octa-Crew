8a783edff638206d24ed03c37e20db94a1f670fd
kamiesull <kamiesull10@gmail.com>
2024-11-09 10:00:42 -0800

    Added test Set_Currency_Invalid_String_Lowercase_Should_Cause_Validation_Error to ProductModel.Tests.cs

--- UnitTests/Models/ProductModel.Tests.cs
+++ UnitTests/Models/ProductModel.Tests.cs
@@ -689,6 +689,51 @@ namespace UnitTests.Models
             Assert.AreEqual("The Currency should have a length of 3", validationResults[0].ErrorMessage);
         }
 
+        /// <summary>
+        /// Setting Currency to string with lowercase letters should cause a validation error
+        /// </summary>
+        [Test]
+        public void Set_Currency_Invalid_String_Lowercase_Should_Cause_Validation_Error()
+        {
+            // Arrange
+            var data = new ProductModel()
+            {
+                Id = System.Guid.NewGuid().ToString(),
+                Images = new string[]
+                {
+                    "https://images.pexels.com/photos/1308940/pexels-photo-1308940.jpeg",
+                    "https://images.pexels.com/photos/2363/france-landmark-lights-night.jpg",
+                    "https://images.pexels.com/photos/161901/paris-sunset-france-monument-161901.jpeg",
+                },
+                Title = "Enter City Name",
+                Description = "Enter City Description",
+                BestSeason = null,
+                Currency = "CUr",
+                TimeZone = "Enter Time Zone",
+                Attractions = new string[3]
+                {
+                    "Enter an Attraction",
+                    "Enter an Attraction",
+                    "Enter an Attraction"
+                },
+                Cost = 0,
+                TravelTime = 0.0,
+                Ratings = null
+            };
+            var validationResults = new List<ValidationResult>();
+
+            // Act
+            bool result = Validator.TryValidateObject(
+                data, new ValidationContext(data), validationResults, true
+            );
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(false, result);
+            Assert.AreEqual("Currency must be in all caps", validationResults[0].ErrorMessage);
+        }
+
         #endregion Currency
 
         #region GetCityRating
