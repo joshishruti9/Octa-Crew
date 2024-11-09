275d382e1b75649d57e0b930f9ba9f894058f935
kamiesull <kamiesull10@gmail.com>
2024-11-09 10:48:30 -0800

    Moved Set_ProductModel_Valid_Null_Ratings_Should_Be_Validated to Ratings region in ProductModel.Tests.cs

--- UnitTests/Models/ProductModel.Tests.cs
+++ UnitTests/Models/ProductModel.Tests.cs
@@ -13,51 +13,6 @@ namespace UnitTests.Models
         #region Images
 
         /// <summary>
-        /// Setting Product with all valid attributes should be successul
-        /// </summary>
-        [Test]
-        public void Set_ProductModel_Valid_Should_Be_Validated()
-        {
-            // Arrange
-            var data = new ProductModel()
-            {
-                Id = System.Guid.NewGuid().ToString(),
-                Images = new string[]
-                {
-                    "https://images.pexels.com/photos/1308940/pexels-photo-1308940.jpeg",
-                    "https://images.pexels.com/photos/2363/france-landmark-lights-night.jpg",
-                    "https://images.pexels.com/photos/161901/paris-sunset-france-monument-161901.jpeg",
-                },
-                Title = "Enter City Name",
-                Description = "Enter City Description",
-                BestSeason = null,
-                Currency = "CUR",
-                TimeZone = "GMT+00",
-                Attractions = new string[3]
-                {
-                    "Enter an Attraction",
-                    "Enter an Attraction",
-                    "Enter an Attraction"
-                },
-                Cost = 0,
-                TravelTime = 0.0,
-                Ratings = null
-            };
-            var validationResults = new List<ValidationResult>();
-
-            // Act
-            bool result = Validator.TryValidateObject(
-                data, new ValidationContext(data), validationResults, true
-            );
-
-            // Reset
-
-            // Assert
-            Assert.AreEqual(true, result);
-            Assert.AreEqual(0, validationResults.Count);
-        }
-
-        /// <summary>
         /// Setting Images to null should cause a validation error
         /// </summary>
         [Test]
@@ -1157,6 +1112,51 @@ namespace UnitTests.Models
             // Assert
             Assert.AreEqual(false, result);
             Assert.AreEqual("Rating #3 must be at most 5", validationResults[0].ErrorMessage);
+        }
+
+        /// <summary>
+        /// Setting Product with all valid attributes and null Ratings should be successul
+        /// </summary>
+        [Test]
+        public void Set_ProductModel_Valid_Null_Ratings_Should_Be_Validated()
+        {
+            // Arrange
+            var data = new ProductModel()
+            {
+                Id = System.Guid.NewGuid().ToString(),
+                Images = new string[]
+                {
+                    "https://images.pexels.com/photos/1308940/pexels-photo-1308940.jpeg",
+                    "https://images.pexels.com/photos/2363/france-landmark-lights-night.jpg",
+                    "https://images.pexels.com/photos/161901/paris-sunset-france-monument-161901.jpeg",
+                },
+                Title = "Enter City Name",
+                Description = "Enter City Description",
+                BestSeason = null,
+                Currency = "CUR",
+                TimeZone = "GMT+00",
+                Attractions = new string[3]
+                {
+                    "Enter an Attraction",
+                    "Enter an Attraction",
+                    "Enter an Attraction"
+                },
+                Cost = 0,
+                TravelTime = 0.0,
+                Ratings = null
+            };
+            var validationResults = new List<ValidationResult>();
+
+            // Act
+            bool result = Validator.TryValidateObject(
+                data, new ValidationContext(data), validationResults, true
+            );
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(0, validationResults.Count);
         }
 
         #endregion Ratings
