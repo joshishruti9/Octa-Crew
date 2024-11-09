31c60f80278bec71cb18e693832f7f50bac5685e
kamiesull <kamiesull10@gmail.com>
2024-11-09 10:47:00 -0800

    Added Set_Ratings_Invalid_Greater_Than_5_Should_Cause_Validation_Error to ProductModel.Tests.cs

--- UnitTests/Models/ProductModel.Tests.cs
+++ UnitTests/Models/ProductModel.Tests.cs
@@ -1114,6 +1114,51 @@ namespace UnitTests.Models
             Assert.AreEqual("Rating #1 must be at least 1", validationResults[0].ErrorMessage);
         }
 
+        /// <summary>
+        /// Setting Ratings to an array with values greater than 5 should cause a validation error
+        /// </summary>
+        [Test]
+        public void Set_Ratings_Invalid_Greater_Than_5_Should_Cause_Validation_Error()
+        {
+            // Arrange
+            var data = new ProductModel()
+            {
+                Id = System.Guid.NewGuid().ToString(),
+                Images = new string[]
+                {
+                    "https://images.pexels.com/photos/1308940/pexels-photo-1308940.jpeg",
+                    "https://images.pexels.com/photos/2363/france-landmark-lights-night.jpg",
+                    "https://images.pexels.com/photos/161901/paris-sunset-france-monument-161901.jpeg",
+                },
+                Title = "Enter City Name",
+                Description = "Enter City Description",
+                BestSeason = null,
+                Currency = "CUR",
+                TimeZone = "GMT-00",
+                Attractions = new string[3]
+                {
+                    "Enter an Attraction",
+                    "Enter an Attraction",
+                    "Enter an Attraction"
+                },
+                Cost = 0,
+                TravelTime = 0.0,
+                Ratings = new int[3] { 1, 5, 6 }
+            };
+            var validationResults = new List<ValidationResult>();
+
+            // Act
+            bool result = Validator.TryValidateObject(
+                data, new ValidationContext(data), validationResults, true
+            );
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(false, result);
+            Assert.AreEqual("Rating #3 must be at most 5", validationResults[0].ErrorMessage);
+        }
+
         #endregion Ratings
 
         #region GetCityRating
