d14ffe5af00e2d764318f83aa71a18e542c3007a
kamiesull <kamiesull10@gmail.com>
2024-11-09 11:02:34 -0800

    Added Set_Cost_Invalid_Greater_Than_9999_Should_Cause_Validation_Error to ProductModel.Tests.cs

--- UnitTests/Models/ProductModel.Tests.cs
+++ UnitTests/Models/ProductModel.Tests.cs
@@ -1253,6 +1253,51 @@ namespace UnitTests.Models
             Assert.AreEqual("Cost should be between 0 and 9999", validationResults[0].ErrorMessage);
         }
 
+        /// <summary>
+        /// Setting Cost to a number greater than 9999 should cause a validation error
+        /// </summary>
+        [Test]
+        public void Set_Cost_Invalid_Greater_Than_9999_Should_Cause_Validation_Error()
+        {
+            // Arrange
+            var data = new ProductModel()
+            {
+                Id = System.Guid.NewGuid().ToString(),
+                Images = new string[]
+                {
+                    "https://images.pexels.com/photos/1308940/pexels-photo-1308940.jpeg",
+                    "https://images.pexels.com/photos/2363/france-landmark-lights-night.jpg",
+                    "https://images.pexels.com/photos/161901/paris-sunset-france-monument-161901.jpeg",
+                },
+                Title = "Enter City Name",
+                Description = "Enter City Description",
+                BestSeason = null,
+                Currency = "CUR",
+                TimeZone = "GMT-00",
+                Attractions = new string[3]
+                {
+                    "Enter an Attraction",
+                    "Enter an Attraction",
+                    "Enter an Attraction"
+                },
+                Cost = 10000,
+                TravelTime = 0.0,
+                Ratings = null
+            };
+            var validationResults = new List<ValidationResult>();
+
+            // Act
+            bool result = Validator.TryValidateObject(
+                data, new ValidationContext(data), validationResults, true
+            );
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(false, result);
+            Assert.AreEqual("Cost should be between 0 and 9999", validationResults[0].ErrorMessage);
+        }
+
         #endregion Cost
 
         #region GetCityRating
