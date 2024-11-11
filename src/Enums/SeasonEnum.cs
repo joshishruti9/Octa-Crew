8e4f61649f1f4a048c54fddb04cc5ad0529b6ff9
kamiesull <kamiesull10@gmail.com>
2024-11-11 10:34:27 -0800

    Set up SeasonEnum in SeasonEnum.cs

--- src/Enums/SeasonEnum.cs
+++ src/Enums/SeasonEnum.cs
@@ -1,6 +1,14 @@
 ﻿namespace ContosoCrafts.WebSite.Enums
 {
-    public class SeasonEnum
+    /// <summary>
+    /// enum to restrict values in BestSeason
+    /// </summary>
+    public enum SeasonEnum
     {
+        Unknown = 0, // Unknown, not specified
+        Spring = 1, // March-May
+        Summer = 2, // June-August
+        Fall = 3, // September-November
+        Winter = 4 // December-February
     }
 }
