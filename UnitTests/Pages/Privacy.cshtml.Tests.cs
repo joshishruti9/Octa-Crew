7dc48ffc7941c3eaced5be21e2b5c511195e602b
Nathan Swetzof <nswetzof@gmail.com>
2024-11-11 20:46:37 -0800

    Added comments to unit tests in Privacy.cshtml.Tests.cs

--- UnitTests/Pages/Privacy.cshtml.Tests.cs
+++ UnitTests/Pages/Privacy.cshtml.Tests.cs
@@ -8,6 +8,9 @@ using ContosoCrafts.WebSite.Pages;
 
 namespace UnitTests.Pages
 {
+    /// <summary>
+    /// Unit testing class for the PrivacyModel class
+    /// </summary>
     public class PrivacyTests
     {
         #region TestSetup
@@ -15,6 +18,10 @@ namespace UnitTests.Pages
         // page model for the Privacy page
         public static PrivacyModel pageModel;
 
+        /// <summary>
+        /// Called before each test is called.
+        /// Sets up necessary test context or variables
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
@@ -31,6 +38,10 @@ namespace UnitTests.Pages
         #endregion TestSetup
 
         #region OnGet
+
+        /// <summary>
+        /// Test that OnGet generates a page model with a valid state
+        /// </summary>
         [Test]
         public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
         {
