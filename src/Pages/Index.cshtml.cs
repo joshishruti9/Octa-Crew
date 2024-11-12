48151cdd091f0324c70626a620fee533073baee5
Nathan Swetzof <nswetzof@gmail.com>
2024-11-11 20:21:17 -0800

    Added comments for IndexModel in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -3,8 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Model for the Index page
+    /// </summary>
     public class IndexModel : PageModel
     {
+        /// <summary>
+        /// REST GET request called when the page is accessed
+        /// </summary>
         public void OnGet()
         {
         }
