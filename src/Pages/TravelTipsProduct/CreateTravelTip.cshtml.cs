13f0a6d06dc3dfea4a8fde2af34205e481fc31df
Nathan Swetzof <nswetzof@gmail.com>
2024-11-14 22:03:08 -0800

    Added default constructor to CreateTravelTip.cshtml.cs

--- src/Pages/TravelTipsProduct/CreateTravelTip.cshtml.cs
+++ src/Pages/TravelTipsProduct/CreateTravelTip.cshtml.cs
@@ -1,10 +1,28 @@
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages.TravelTipsProduct
 {
+    /// <summary>
+    /// Model for the CreateTravelTip page.  Uses input data to add a new travel tip to the database.
+    /// </summary>
     public class CreateTravelTipModel : PageModel
     {
+        // Accesses travel tip data from the database
+        public JsonFileTravelTipService TravelTipService { get; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public CreateTravelTipModel(JsonFileTravelTipService travelTipService)
+        {
+            TravelTipService = travelTipService;
+        }
+
+        /// <summary>
+        /// REST Get request initializes the data
+        /// </summary>
         public void OnGet()
         {
         }
