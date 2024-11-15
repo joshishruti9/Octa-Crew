2d7ae92ca3c264bf1439912da4e3f2236c9ae6d1
Nathan Swetzof <nswetzof@gmail.com>
2024-11-14 22:44:02 -0800

    Added unit test for OnGet in CreateTravelTip.cshtml.Tests.cs

--- UnitTests/Pages/TravelTipsProduct/CreateTravelTip.cshtml.Tests.cs
+++ UnitTests/Pages/TravelTipsProduct/CreateTravelTip.cshtml.Tests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Pages.TravelTipsProduct;
 using ContosoCrafts.WebSite.Services;
 using NUnit.Framework;
@@ -16,8 +17,11 @@ namespace UnitTests.Pages.TravelTipsProduct
     {
         #region TestSetup
 
-        // page model for the CRUDi travel tips delete page
+        // page model for the CRUDi CreateTravelTip page
         public static CreateTravelTipModel pageModel;
+
+        // TravelTipsModel object with default values filled in
+        public static TravelTipsModel defaultModel;
 
         // mock service for the travel tips database
         public static JsonFileTravelTipService TravelTipService;
@@ -31,8 +35,36 @@ namespace UnitTests.Pages.TravelTipsProduct
         {
             TravelTipService = TestHelper.TravelTipService;
             pageModel = new CreateTravelTipModel(TravelTipService);
+            defaultModel = new TravelTipsModel()
+            {
+                Id = System.Guid.NewGuid().ToString(),
+                Title = null,
+                Description = null
+            };
         }
 
         #endregion TestSetup
+
+        #region OnGet
+
+        /// <summary>
+        /// Test that OnGet correctly sets the page's pageModel values to the defaults
+        /// </summary>
+        [Test]
+        public void OnGet_Valid_Default_Should_Set_Default_Values()
+        {
+            // Arrange
+            var data = defaultModel;
+
+            // Act
+            pageModel.OnGet();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(data.Title, defaultModel.Title);
+            Assert.AreEqual(data.Description, defaultModel.Description);
+        }
+
+        #endregion OnGet
     }
 }
