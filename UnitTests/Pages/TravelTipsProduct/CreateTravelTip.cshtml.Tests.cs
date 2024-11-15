e53eb907e657c9abab14ce8f994ab1c4e607b222
Nathan Swetzof <nswetzof@gmail.com>
2024-11-14 22:18:47 -0800

    Added setup code to CreateTravelTip.cshtml.Tests.cs

--- UnitTests/Pages/TravelTipsProduct/CreateTravelTip.cshtml.Tests.cs
+++ UnitTests/Pages/TravelTipsProduct/CreateTravelTip.cshtml.Tests.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Pages.TravelTipsProduct;
+using ContosoCrafts.WebSite.Services;
+using NUnit.Framework;
 
 namespace UnitTests.Pages.TravelTipsProduct
 {
-    class CreateTravelTip
+    /// <summary>
+    /// Unit testing class for the create page model for travel tips
+    /// </summary>
+    class CreateTravelTipTests
     {
+        #region TestSetup
+
+        // page model for the CRUDi travel tips delete page
+        public static CreateTravelTipModel pageModel;
+
+        // mock service for the travel tips database
+        public static JsonFileTravelTipService TravelTipService;
+
+        /// <summary>
+		/// Called before each test is called.
+		/// Sets up necessary test context or variables
+		/// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            TravelTipService = TestHelper.TravelTipService;
+            pageModel = new CreateTravelTipModel(TravelTipService);
+        }
+
+        #endregion TestSetup
     }
 }
