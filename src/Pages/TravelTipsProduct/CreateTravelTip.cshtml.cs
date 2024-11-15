2b8fd01cd22b9f88c6f3637026ca0dcb1e63f3b6
Nathan Swetzof <nswetzof@gmail.com>
2024-11-14 21:54:30 -0800

    Added CreateTravelTip.cshtml and CreateTravelTip.cshtml.cs to TravelTipsProduct folder.

---
+++ src/Pages/TravelTipsProduct/CreateTravelTip.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.TravelTipsProduct
+{
+    public class CreateTravelTipModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
