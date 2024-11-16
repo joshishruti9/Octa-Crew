9e85a4fed6a269eadc1bb093602ccbd18819178d
Nathan Swetzof <nswetzof@gmail.com>
2024-11-16 15:36:37 -0800

    Added Setup method to Map.cshtml.Tests.cs

--- UnitTests/Pages/Product/Map.cshtml.Tests.cs
+++ UnitTests/Pages/Product/Map.cshtml.Tests.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using ContosoCrafts.WebSite.Pages.Product;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace UnitTests.Pages.Product
 {
-    class Map
+    /// <summary>
+    /// Unit testing class for Map page model
+    /// </summary>
+    class MapTests
     {
+        #region TestSetup
+
+        // Page model for the Map page
+        public static MapModel pageModel;
+
+        /// <summary>
+        /// Called before each test is called.
+        /// Sets up necessary test context or variables
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            pageModel = new MapModel(TestHelper.ProductService);
+        }
+
+        #endregion TestSetup
     }
 }
