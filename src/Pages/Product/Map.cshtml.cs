f56650735ec9eb8b3176763f4a459bc66adc3842
Nathan Swetzof <nswetzof@gmail.com>
2024-11-16 09:41:27 -0800

    Added Map.cshtml and Map.cshtml.cs to Product folder

---
+++ src/Pages/Product/Map.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    public class MapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
