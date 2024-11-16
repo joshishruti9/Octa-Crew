a95aea1db5a8959d0703c78aba10ccddfa0a62fe
Nathan Swetzof <nswetzof@gmail.com>
2024-11-15 19:31:20 -0800

    Added unit tests for OnPost in CreateTravelTip.cshtml.Tests.cs

--- UnitTests/Pages/TravelTipsProduct/CreateTravelTip.cshtml.Tests.cs
+++ UnitTests/Pages/TravelTipsProduct/CreateTravelTip.cshtml.Tests.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Pages.TravelTipsProduct;
 using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using NUnit.Framework;
 
 namespace UnitTests.Pages.TravelTipsProduct
@@ -66,5 +68,70 @@ namespace UnitTests.Pages.TravelTipsProduct
         }
 
         #endregion OnGet
+
+        #region OnPost
+
+        /// <summary>
+        /// Test that a city is not added to the database if the ModelState is invalid
+        /// </summary>
+        [Test]
+        public void OnPost_InValid_Invalid_ModelState_Should_Not_Add_Travel_Tip()
+        {
+            // Arrange
+            pageModel.ModelState.AddModelError("Title", "Invalid title");
+
+            // number of travel tips in the database before calling OnPost
+            var countOriginal = TravelTipService.GetAllData().Count();
+
+            // Act
+            var result = pageModel.OnPost();
+
+            // Assert
+            Assert.AreEqual(true, result.GetType().Equals(typeof(PageResult)));
+            Assert.AreEqual(countOriginal, TravelTipService.GetAllData().Count());
+        }
+
+
+        /// <summary>
+        /// Test that OnPost successfully adds a city to the database when custom 
+        /// values are passed in.
+        /// </summary>
+        [Test]
+        public void OnPost_Valid_Custom_Should_Add_Travel_Tip()
+        {
+            // Arrange
+            pageModel.PageModel = new TravelTipsModel()
+            {
+                Id = System.Guid.NewGuid().ToString(),
+                Title = "New Title",
+                Description = "New Description"
+            };
+
+            // the TravelTipsModel for the page
+            var data = pageModel.PageModel;
+
+            // travel tips in the database before calling OnPost
+            var dataOriginalList = TravelTipService.GetAllData();
+
+            // Act
+            var result = pageModel.OnPost();
+
+            // the travel tips in the database after calling OnPost
+            var dataNewList = TravelTipService.GetAllData();
+
+            // the new travel tip added by OnPost
+            var newTravelTip = TravelTipService.GetAllData().First(x => x.Id.Equals(data.Id));
+
+            // Reset
+            TravelTipService.SaveData(dataOriginalList);
+
+            // Assert
+            Assert.AreEqual(dataOriginalList.Count() + 1, dataNewList.Count());
+            Assert.AreEqual(true, data.ToString().Equals(newTravelTip.ToString()));
+            Assert.AreEqual(true, TestHelper.ModelState.IsValid);
+            Assert.AreEqual("./TravelTips", (result as RedirectToPageResult).PageName);
+        }
+
+        #endregion OnPost
     }
 }
