228c7e6e070b05c2a2e2de9f062fd6718b2e9975
Nathan Swetzof <nswetzof@gmail.com>
2024-11-16 09:57:59 -0800

    Added constructor to Map.cshtml.cs

--- src/Pages/Product/Map.cshtml.cs
+++ src/Pages/Product/Map.cshtml.cs
@@ -1,12 +1,31 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+    /// <summary>
+    /// Model for the Map page.
+    /// </summary>
     public class MapModel : PageModel
     {
+        // Accesses city data from database
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService">Set to ProductService</param>
+        public MapModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
         public void OnGet()
         {
+
         }
     }
 }
