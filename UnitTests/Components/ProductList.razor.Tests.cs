3aae682ecb0b003c9cc944a43ddce1405cc178c8
kamiesull <kamiesull10@gmail.com>
2024-11-18 15:47:15 -0800

    Clean up file ProductList.razor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace UnitTests.Components
 {
-    internal class ProductList
+    public class ProductListTests
     {
     }
 }
