23658ab626f4420378a58970790a6b8781d7cffa
kamiesull <kamiesull10@gmail.com>
2024-11-19 09:23:59 -0800

    Added SubmitRating_Valid_ID_Click_Unstarred_Should_Increment_Count_And_Check_Star to ProductList.razor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -64,5 +64,87 @@ namespace UnitTests.Components
             ));
         }
 
+        [Test]
+        public void SubmitRating_Valid_ID_Click_Unstarred_Should_Increment_Count_And_Check_Star()
+        {
+            /*
+             * This test tests that the SubmitRating will change the vote as well as the Star
+             * checked.
+             * Because the star check is a calculation of the ratings, using a record that has no
+             * stars and checking one makes it clear what was changed.
+             * 
+             * The test needs to open the page,
+             * Then open the popup on the card,
+             * Then record the state of the count and star check status,
+             * Then check a star,
+             * Then check again the state of the count and star check status.
+             */
+
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+            var id = "ExploreMoreButton_rome";
+
+            var page = RenderComponent<ProductList>();
+
+            // Find the Buttons (Explore More)
+            var buttonList = page.FindAll("Button");
+
+            // Find the one that matches the ID looking for and click it
+            var button = buttonList.First(m => m.OuterHtml.Contains(id));
+            button.Click();
+
+            // Get the markup of the page post the Click action
+            var buttonMarkup = page.Markup;
+
+            // Get the Star Buttons
+            var starButtonList = page.FindAll("span");
+
+            // Get the Vote Count, the List should have 7 elements,
+            // element 2 is the string for the count
+            var preVoteCountSpan = starButtonList[1];
+            var preVoteCountString = preVoteCountSpan.OuterHtml;
+
+            // Get the First star item from the list, it should not be checked
+            var starButton = starButtonList.First(m =>
+                string.IsNullOrEmpty(m.ClassName) == false
+                && m.ClassName.Contains("fa fa-star")
+            );
+
+            // Save the html for it to compare after the click
+            var preStarChange = starButton.OuterHtml;
+
+            // Act
+
+            // Click the star button
+            starButton.Click();
+
+            // Get the markup to use for the assert
+            buttonMarkup = page.Markup;
+
+            // Get the Star Buttons
+            starButtonList = page.FindAll("span");
+
+            // Get the Vote Count, the List should have 7 elements, element 2 is the string for the count
+            var postVoteCountSpan = starButtonList[1];
+            var postVoteCountString = postVoteCountSpan.OuterHtml;
+
+            // Get the Last starred item from the list
+            starButton = starButtonList.First(m =>
+                string.IsNullOrEmpty(m.ClassName) == false
+                && m.ClassName.Contains("fa fa-star checked")
+            );
+
+            // Save the html for it to compare after the click
+            var postStarChange = starButton.OuterHtml;
+
+            // Reset
+
+            // Assert
+
+            // Confirm that the record had no votes to start, and 1 vote after
+            Assert.AreEqual(true, preVoteCountString.Contains("Be the first to vote!"));
+            Assert.AreEqual(true, postVoteCountString.Contains("1 Vote"));
+            Assert.AreEqual(false, preVoteCountString.Equals(postVoteCountString));
+        }
     }
 }
