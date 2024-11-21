9793134d703fbfaf844301c13b820f6af8f6c795
kamiesull <kamiesull10@gmail.com>
2024-11-21 11:41:56 -0800

    Updated ValidationResult return statement in ImageURLValidation.cs

--- src/ValidationAttributes/ImageURLValidation.cs
+++ src/ValidationAttributes/ImageURLValidation.cs
@@ -2,45 +2,54 @@
 
 namespace ContosoCrafts.WebSite.ValidationAttributes
 {
-	/// <summary>
-	/// Custom validation attribute for Images
-	/// </summary>
-	public class ImageURLValidation
-	{
-		/// <summary>
-		/// Method for validating URLs in Images
-		/// </summary>
-		/// <param name="URLs">Array of URLs to be validated</param>
-		/// <returns>Success if URLs are valid, ValidationResult if any URL is not valid</returns>
-		public static ValidationResult ImageURLValidate(string[] URLs)
-		{
-			// Checks if URL is valid
-			UrlAttribute urlCheck = new UrlAttribute();
+    /// <summary>
+    /// Custom validation attribute for Images
+    /// </summary>
+    public class ImageURLValidation
+    {
+        /// <summary>
+        /// Method for validating URLs in Images
+        /// </summary>
+        /// <param name="URLs">Array of URLs to be validated</param>
+        /// <returns>Success if URLs are valid, ValidationResult if any URL is not valid</returns>
+        public static ValidationResult ImageURLValidate(string[] URLs)
+        {
+            // Checks if URL is valid
+            UrlAttribute urlCheck = new UrlAttribute();
 
-			// Checks if URL ends in valid image extension
-			RegularExpressionAttribute regEx = new RegularExpressionAttribute(
-				@".*\.(jpg|jpeg|png|gif|bmp|svg|webp)$"
-			);
+            // Checks if URL ends in valid image extension
+            RegularExpressionAttribute regEx = new RegularExpressionAttribute(
+                @".*\.(jpg|jpeg|png|gif|bmp|svg|webp)$"
+            );
 
-			for (int i = 0; i < URLs.Length; i++)
-			{
-				if (string.IsNullOrEmpty(URLs[i]))
-				{
-					return new ValidationResult($"Image #{i + 1} URL is empty");
-				}
+            for (int i = 0; i < URLs.Length; i++)
+            {
+                if (string.IsNullOrEmpty(URLs[i]))
+                {
+                    return new ValidationResult(
+                        $"Image #{i + 1} URL is empty",
+                        new[] { $"[{i}]" }
+                    );
+                }
 
-				if (urlCheck.IsValid(URLs[i]) == false)
-				{
-					return new ValidationResult($"Image #{i + 1} URL is not valid");
-				}
+                if (urlCheck.IsValid(URLs[i]) == false)
+                {
+                    return new ValidationResult(
+                        $"Image #{i + 1} URL is not valid",
+                        new[] { $"[{i}]" }
+                    );
+                }
 
-				if (regEx.IsValid(URLs[i]) == false)
-				{
-					return new ValidationResult($"Image #{i + 1} URL does not end in image extension");
-				}
-			}
+                if (regEx.IsValid(URLs[i]) == false)
+                {
+                    return new ValidationResult(
+                        $"Image #{i + 1} URL does not end in image extension",
+                        new[] { $"[{i}]" }
+                    );
+                }
+            }
 
-			return ValidationResult.Success;
-		}
-	}
+            return ValidationResult.Success;
+        }
+    }
 }
