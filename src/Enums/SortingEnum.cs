530f40ee7c1cf46593afd21ee30c259a192ad6ba
Nathan Swetzof <nswetzof@gmail.com>
2024-11-22 15:17:08 -0800

    Added comments to SortingEnumExtensions in SortingEnum.cs

--- src/Enums/SortingEnum.cs
+++ src/Enums/SortingEnum.cs
@@ -9,8 +9,16 @@
         Rating = 10
     }
 
+    /// <summary>
+    /// Extension to SortingEnum for displaying custom text for each value
+    /// </summary>
     public static class SortingEnumExtensions
     {
+        /// <summary>
+        /// Converts enum value to custom string
+        /// </summary>
+        /// <param name="data">The enum value to display</param>
+        /// <returns>String representation of the enum value</returns>
         public static string DisplayName(this SortingEnum data)
         {
             return data switch
