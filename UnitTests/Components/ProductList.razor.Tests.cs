30eeb1c24bfb3a2aa059804aa205e70f56dc2946
kamiesull <kamiesull10@gmail.com>
2024-11-27 12:13:50 -0800

    Added GetFilteredProducts_Filter_By_TravelTime_Null_Should_Return_All_Cities to ProductList.razor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -673,6 +673,41 @@ namespace UnitTests.Components
             Assert.AreEqual(false, pageMarkup.Contains("card_new-york-city"));
         }
 
+        /// <summary>
+        /// Setting maximum travel time to null should return all products
+        /// </summary>
+        [Test]
+        public void GetFilteredProducts_Filter_By_TravelTime_Null_Should_Return_All_Cities()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+
+            // Render the ProductList component
+            var page = RenderComponent<ProductList>();
+
+            // Act
+
+            // Set the maximum travel time to null
+            var searchBox = page.Find("input[placeholder='Enter Maximum Travel Time...']");
+            searchBox.Change(null as string);
+
+            // Get the rendered markup
+            var pageMarkup = page.Markup;
+
+            // Reset
+
+            // Assert
+
+            // Check for cities with lowest and highest travel times
+            // to ensure the entire range of travel times is being returned
+
+            // Check that Vancouver appears in the results (lowest travel time)
+            Assert.AreEqual(true, pageMarkup.Contains("card_vancouver"));
+
+            // Check that Cape Town appears (highest travel time)
+            Assert.AreEqual(true, pageMarkup.Contains("card_cape-town"));
+        }
+
         #endregion Filter
 
         #region GetSortedProducts
