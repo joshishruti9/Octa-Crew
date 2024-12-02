87f7c7f1ccf4096e0654ca42bc62591dc9ab4d12
Shruti Joshi <joshi2312shruti@gmail.com>
2024-12-03 04:15:33 +0530

    Added correct spaces in Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -2,16 +2,20 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+
     /// <summary>
     /// Model for the Index page
     /// </summary>
     public class IndexModel : PageModel
     {
+
         /// <summary>
         /// REST GET request called when the page is accessed
         /// </summary>
         public void OnGet()
         {
+
         }
     }
+
 }
